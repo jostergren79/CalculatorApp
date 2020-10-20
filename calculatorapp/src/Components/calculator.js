492e7988c56e7ac135637eea9545d425c42079e7
Jason Ostergren <jason.ostergren79@gmail.com>
2020-10-20 09:31:35 -0500

    adding email registration compononent

--- calculatorapp/src/Components/calculator.js
+++ calculatorapp/src/Components/calculator.js
@@ -61,14 +61,67 @@ class Calculator extends Component {
     render() {
         return (
             <div>
-                <div className="calculator-body">
+                <div> className="calculator-body">
                     <h1>Simple Calculator</h1>
                     <OutputScreen result={this.state.result}/>
                     <Button onClick={this.onClick}/>
                 </div>
-            </div>
-        );
-    }
+			
+			<form>
+					className="ui form"
+					id={this.props.id}
+					name={this.props.name}
+					method={this.props.method}
+					action={this.props.action}
+					/>
+		  	<textarea
+					id="name"
+					name="name"
+					onChange={this.handleInputChange.bind(this)}
+					placeholder="your name"
+					required
+					value={this.state.name}
+					style={{ width: "100%" }}
+					rows={1}
+		  			/>
+		 	<br />
+		  	<textarea
+					id="email"
+					name="email"
+					onChange={this.handleInputChange.bind(this)}
+					placeholder="your email address"
+					required
+					value={this.state.email}
+					error={this.state.errors.email}
+					style={{ width: "100%" }}
+					rows={1}
+		 			 />
+		  	<br />
+		  	<textarea
+					id="feedback"
+					name="feedback"
+					onChange={this.handleInputChange.bind(this)}
+					placeholder="what would you like to chat about?"
+					required
+					value={this.state.feedback}
+					style={{ width: "100%", height: "250px" }}
+		 			/>
+		    <br />
+		  	<input
+					type="button"
+					value="Send"
+					className="ui button"
+					style={{
+					fontFamily: "Amatic SC",
+					fontSize: "20px",
+					color: "blue"
+					}}
+					onClick={this.sendMessage.bind(this)}
+		  			/>
+			</form>
+	  </div>
+	);
+  };
 }
 
 export default Calculator;
---
+++ calculatorapp/src/Components/emailregister.js
@@ -0,0 +1,111 @@
+
+
+export default class extends React.Component {
+    constructor(props) {
+      super(props);
+  
+      this.state = {
+        name: "",
+        email: "",
+        feedback: "",
+      };
+    }
+  
+    handleInputChange(event) {
+      event.preventDefault();
+      const target = event.target;
+      const name = target.name;
+      const value = target.value;
+  
+      this.setState({ [name]: value });
+    }
+    render() {
+        return (
+          <div>
+            <form
+              className="ui form"
+              id={this.props.id}
+              name={this.props.name}
+              method={this.props.method}
+              action={this.props.action}
+            >
+              <textarea
+                id="name"
+                name="name"
+                onChange={this.handleInputChange.bind(this)}
+                placeholder="your name"
+                required
+                value={this.state.name}
+                style={{ width: "100%" }}
+                rows={1}
+              />
+              <br />
+              <textarea
+                id="email"
+                name="email"
+                onChange={this.handleInputChange.bind(this)}
+                placeholder="your email address"
+                required
+                value={this.state.email}
+                error={this.state.errors.email}
+                style={{ width: "100%" }}
+                rows={1}
+              />
+              <br />
+              <textarea
+                id="feedback"
+                name="feedback"
+                onChange={this.handleInputChange.bind(this)}
+                placeholder="what would you like to chat about?"
+                required
+                value={this.state.feedback}
+                style={{ width: "100%", height: "250px" }}
+              />
+              <br />
+              <input
+                type="button"
+                value="Send"
+                className="ui button"
+                style={{
+                  fontFamily: "Amatic SC",
+                  fontSize: "20px",
+                  color: "blue"
+                }}
+                onClick={this.sendMessage.bind(this)}
+              />
+            </form>
+          </div>
+        );
+      };
+
+sendMessage(event) {
+
+    event.preventDefault();
+
+    if (!this.validateMail()) {
+          return;
+        }
+    const templateParams = {
+          from_name: this.state.name + " (" + this.state.email + ")",
+          to_name: {wherever you are sending the email},
+          feedback: this.state.feedback
+        };
+    emailjs
+          .send("gmail", "portfoliositecontact", templateParams, {id given from your EmailJS template})
+          .then(
+            function(response) {
+              toast.success("Your message has successfully sent!", {
+                position: toast.POSITION.BOTTOM_CENTER
+              });
+              console.log("SUCCESS!", response.status, response.text);
+            },
+            function(err) {
+              toast.error("Your message was not able to be sent");
+            }
+          );
+    this.setState({
+          name: "",
+          email: "",
+          feedback: ""
+        });
+      }}
