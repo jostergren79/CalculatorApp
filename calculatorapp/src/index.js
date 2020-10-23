2261e04411a254d0abfaf1f21d4ae708826f9ed8
Jason Ostergren <jason.ostergren79@gmail.com>
2020-10-23 09:29:30 -0500

    updated layout to include server and additional librarys, babel, webpack

--- calculatorapp/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'; 
-import ReactDOM from 'react-dom'; 
-import App from './Components/App'; 
-
-// Render the Calculator to the Web page. 
-ReactDOM.render(<App />, document.getElementById('root')); 
